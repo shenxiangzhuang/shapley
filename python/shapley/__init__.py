e13242ec426662f8cb3e6b8b847b75fbab0442b7
Xiangzhuang Shen <datahonor@gmail.com>
2024-11-13 16:42:39 +0800

    maturin init: maturin new

---
+++ python/shapley/__init__.py
@@ -0,0 +1,6 @@
+from .shapley import *
+
+
+__doc__ = shapley.__doc__
+if hasattr(shapley, "__all__"):
+    __all__ = shapley.__all__
---
+++ python/tests/test_all.py
@@ -0,0 +1,6 @@
+import pytest
+import shapley
+
+
+def test_sum_as_string():
+    assert shapley.sum_as_string(1, 1) == "2"
