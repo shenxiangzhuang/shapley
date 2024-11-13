48037df0290cd8d186d291dbe7d13a6a9d9146fd
Xiangzhuang Shen <datahonor@gmail.com>
2024-11-13 16:58:28 +0800

    init

---
+++ src/shapley.rs
@@ -0,0 +1,10 @@
+fn score(v: &Vec<u32>) -> f64 {
+    // todo: implement this
+    0.0
+}
+
+
+pub fn shapley(n_member: u32, score: &dyn Fn(&Vec<u32>) -> f64) -> Vec<f64> {
+    // todo: implement this
+    0.0
+}
