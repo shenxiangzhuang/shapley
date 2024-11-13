bf58e6d1fe72d081d6633d57921aae8587cba6e7
Xiangzhuang Shen <datahonor@gmail.com>
2024-11-13 17:16:27 +0800

    add: ref

--- src/shapley.rs
+++ src/shapley.rs
@@ -1,3 +1,6 @@
+/// Shapley value calculation
+/// Lloyd Shapley https://www.nobelprize.org/prizes/economic-sciences/2012/shapley/facts/
+
 fn score(v: &Vec<u32>) -> f64 {
     // todo: implement this
     0.0
