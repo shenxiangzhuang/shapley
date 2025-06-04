f8534ab4a735546cc97cad21a3d66ab1e8257300
Xiangzhuang Shen <datahonor@gmail.com>
2025-06-04 16:56:08 +0800

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-mod shapley;
-
 use pyo3::prelude::*;
 
 /// Formats the sum of two numbers as string.
@@ -10,7 +8,7 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn shapley_pyo3(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn shapley(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
 }
