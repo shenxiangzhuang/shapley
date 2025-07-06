f5d83676647b03eeb9f80a060c68c3ab264f56a5
Mathew Shen <datahonor@gmail.com>
2025-07-06 11:14:39 +0800

    refactor(export): re-export the structs (#6)

--- bindings/python/src/lib.rs
+++ bindings/python/src/lib.rs
@@ -4,7 +4,7 @@ use std::collections::HashMap;
 #[derive(Debug)]
 #[pyclass]
 struct Shapley {
-    inner: shapley::shapley::Shapley,
+    inner: shapley::Shapley,
 }
 
 #[pymethods]
@@ -13,11 +13,11 @@ impl Shapley {
     fn py_new(players: Vec<u64>, coalition_worth: HashMap<Vec<u64>, f64>) -> PyResult<Self> {
         let mut converted_worth = HashMap::new();
         for (members, worth) in coalition_worth {
-            let coalition = shapley::shapley::Coalition::new(members);
+            let coalition = shapley::Coalition::new(members);
             converted_worth.insert(coalition, worth);
         }
 
-        let inner = shapley::shapley::Shapley::new(players, converted_worth);
+        let inner = shapley::Shapley::new(players, converted_worth);
         Ok(Self { inner })
     }
 
